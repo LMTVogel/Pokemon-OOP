a1d022f9b234a3fd7bb75e568cd5f9635eefaeb9
Luuk <99051050@mydavinci.nl>
2020-04-07 20:16:37 +0200

    Lab has been finnished.

--- LAB - OOP - Superheroes/index.php
+++ LAB - OOP - Superheroes/index.php
@@ -1,8 +1,11 @@
 <?php 
 require 'Superhero.php';
+require 'Avenger.php';
 
+// Objects
 $spiderman = new Superhero('Spider-Man', 'Male', 'Spiderfriends', 'With great power comes great responsibility!');
+$thor = new Avenger('Thor', 'Male', 'For Asgard');
 
-print_r('<pre>'. $spiderman. '<pre>');
+$thor->sayOneliner();
 
-$spiderman->sayOneliner();
+print_r('<pr>'. $thor. '</pre>');
