69d900d85d42e5a1a13b8a5911cafcfbc16d6edf
Luuk <99051050@mydavinci.nl>
2020-04-13 15:03:21 +0200

    EnergyType class has been made.

--- EnergyType.php
+++ EnergyType.php
@@ -1,1 +1,16 @@
 <?php
+
+class EnergyType 
+{
+    public $name;
+
+    public function __construct ($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName ()
+    {
+        return $this->name;
+    }
+}
