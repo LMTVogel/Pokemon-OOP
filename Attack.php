f71f6eb9537f97984e0ee86a3877d8f9b228d855
Luuk Vogel <99051050@mydavinci.nl>
2020-04-15 01:18:28 +0200

    Attack class has been made.

--- Attack.php
+++ Attack.php
@@ -1,1 +1,33 @@
 <?php
+
+class Attack
+{
+    public $name;
+    public $damage;
+
+    public function __construct($name, $damage)
+    {
+        $this->name = $name;
+        $this->damage = $damage;
+    }
+
+    public function multiplyDamage($multiplier)
+    {
+        $this->damage = $this->damage * $multiplier;
+    }
+
+    public function reduceDamage($resistance)
+    {
+        $this->damage = $this->damage - $resistance;
+    }
+
+    public function getAttackName()
+    {
+        return $this->name;
+    }
+
+    public function getAttackDamage()
+    {
+        return $this->damage;
+    }
+}
