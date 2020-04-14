b323a4c2943c7f0ec646ec4009943af7cf85c47e
Luuk Vogel <99051050@mydavinci.nl>
2020-04-15 01:41:21 +0200

    Pikachu class has been made.

--- Pikachu.php
+++ Pikachu.php
@@ -1,1 +1,19 @@
 <?php
+
+class Pikachu extends \Pokemon\Pokemon
+{
+    public function __construct($name)
+    {
+        $energyType = new EnergyType('Lightning');
+        $hitpoints = 60;
+        $attacks = array(
+            'Electric Ring' => new Attack('Electric Ring', 50),
+            'Pika Punch' => new Attack('Pika Punch', 20)
+        );
+
+        $weakness = new Weakness('Fire', 1.5);
+        $resistance = new Resistance('Fighting', 20);
+
+        parent::__construct($name, $energyType, $hitpoints, $attacks, $weakness, $resistance);
+    }
+}
