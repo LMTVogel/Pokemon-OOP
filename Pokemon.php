bff0ca2b35af42825de309e68ac610060dc2612d
Luuk <99051050@mydavinci.nl>
2020-04-20 01:25:17 +0200

    Added some comments

--- Pokemon.php
+++ Pokemon.php
@@ -33,32 +33,34 @@ class Pokemon
     /* Function gemaakt om de Pokemons elkaar aan te laten vallen */
     public function battleTurn($target, $attacks)
     {
+        /* De variabeles pakken alle informatie die ze nodig voor het gevecht */
         $energyType = $this->getEnergyType()->getName();
         $weaknessEnergyType = $target->getWeakness()->getEnergyType();
         $multiplierEnergyType = $target->getWeakness()->getEnergyTypeValue();
 
         $resistanceEnergyType = $target->getResistance()->getEnergyType();
         $resistance = $target->getResistance()->getEnergyTypeValue();
-
+        /* Laat zien hoeveel hitpoints een Pokemon nog heeft */
         echo "<br><strong>" . $target->getPokemonName() .  " HP: " . $target->getHealth() . "/" .  $target->getHitpoints() . " <br><br>";
+        /* Kijkt of de weakness type hetzelfde is als de aanval type om zo de multiplier te activeren */
         if ($weaknessEnergyType == $energyType) {
             $damage = $attacks->getAttackDamage() * $multiplierEnergyType;
             echo $this->name . " valt aan met " .  $attacks->getAttackName() . "! It's super effective! (" . $damage . " damage)<br>";
-        }
+        } /* Kijkt of de resistance type hetzelfde is als de aanval type om zo de resistance van de aanvals punten af te halen */
         elseif ($resistanceEnergyType == $energyType) {
             $damage = $attacks->getAttackDamage() - $resistance;
             echo $this->name . " valt aan met " .  $attacks->getAttackName() . "! It's not very effective (" . $damage . " damage)<br>";
-        }
+        } /* Als de if en elseif niet overeenkomen dan wordt de aanval met normale values uitgevoerd */
         else {
             $damage = $attacks->getAttackDamage();
             echo $this->name . " valt aan met " .  $attacks->getAttackName() . " (" . $damage . " damage)<br>";
         }
-
+        /* Na elke aanval wordt de aanval value van de health afgetrokken */
         $this->damageDone($damage, $target);
     }
     /* Deze function laat zien hoeveel health de Pokemon nog over heeft of hij laat zien dat hij dood is */
     public function damageDone($damage, $target)
-    {
+    { /*  */
         $target->health -= $damage;
         if ($target->getHealth() <= 0) {
             echo $target->getPokemonName()  .  " fainted!<br>";
