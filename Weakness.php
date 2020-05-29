ad3e54bed073ba637a0e7bf40b37af2b2ce79469
Luuk Vogel <99051050@mydavinci.nl>
2020-05-29 15:15:25 +0200

    Added comments to the weakness class

--- Weakness.php
+++ Weakness.php
@@ -4,18 +4,25 @@ class Weakness
 {
     public $name;
     public $multiplier;
-    /* Constructor wordt uitgevoerd als er een nieuw object wordt aangemaakt */
+    /** Constructor wordt uitgevoerd als er een nieuw object wordt aangemaakt
+     * @param string $name
+     * @param int $multiplier
+     */
     public function __construct($name, $multiplier)
     {
         $this->name = $name;
         $this->multiplier = $multiplier;
     }
-    /* Pakt de naam van de energy type */
+    /** Pakt de naam van de energy type
+     * @return string $name
+     */
     public function getEnergyType()
     {
         return $this->name;
     }
-    /* Pakt de value van de energy type die gebruikt wordt als multiplier */
+    /** Pakt de value van de energy type die gebruikt wordt als multiplier
+     * @return int $multiplier
+     */
     public function getEnergyTypeValue()
     {
         return $this->multiplier;
